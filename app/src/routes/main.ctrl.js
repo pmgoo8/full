344b4d8e73555a8b1c4d3c194e9a93b71c65d5dc
ralgoo <sonjaeyeon43@naver.com>
2022-08-30 18:16:20 +0900

    make model

--- app/src/routes/main.ctrl.js
+++ app/src/routes/main.ctrl.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const UserStorage = require("../model/UserStorage");
+const User = require("../model/User");
 
 const output = {
     main: (req, res) => {
@@ -13,21 +13,9 @@ const output = {
 
 const process = {
     login: (req, res) => {
-        const id = req.body.id,
-            password = req.body.password;
-        const users = UserStorage.getUsers("id", "password");
-
-        const response = {};
-        if(users.id.includes(id)) {
-            const idx = users.id.indexOf(id);
-            if(users.password[idx] === password) {
-                response.success = true;
-                return res.json(response);
-            }
-        }
-
-        response.success = false;
-        response.msg = "로그인데 실패하였습니다."
+        const user = new User(req.body);
+        const response = user.login();
+        console.log(response);
         return res.json(response);
     },
 };
