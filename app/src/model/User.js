d203cfe26a3d41d118d88edabde102c63357e4b5
ralgoo <sonjaeyeon43@naver.com>
2022-09-01 15:13:39 +0900

    DB connect

--- app/src/model/User.js
+++ app/src/model/User.js
@@ -4,32 +4,36 @@ const { response } = require("express");
 const UserStorage = require("./UserStorage");
 
 class User {
-    constructor(body) {
-        this.body = body;
-    }
+  constructor(body) {
+    this.body = body;
+  }
 
-    async login() {
-        const client = this.body;
-        const { id, password } = await UserStorage.getUserInfo(client.id);
+  async login() {
+    const client = this.body;
+    try {
+      const { id, password } = await UserStorage.getUserInfo(client.id);
 
-        if (id) {
-            if (id === client.id && password === client.password) { 
-                return { success: true };
-            }
-            return { success: false, msg: "비밀번호가 일치하지 않습니다."};
+      if (id) {
+        if (id === client.id && password === client.password) {
+          return { success: true };
         }
-        return { success: false, msg: "존재하지 않는 아이디 입니다."};
+        return { success: false, msg: "비밀번호가 틀렸습니다." };
+      }
+      return { success: false, msg: "존재하지 않는 아이디입니다." };
+    } catch (err) {
+      return { success: false, msg: err };
     }
+  }
 
-    async register() {
-        const client = this.body;
-        try {
-            const response = await UserStorage.save(client);
-            return response;
-        } catch (err) {
-            return { success: false, msg: err };
-        }
+  async register() {
+    const client = this.body;
+    try {
+      const response = await UserStorage.save(client);
+      return response;
+    } catch (err) {
+      return { success: false, msg: err };
     }
+  }
 }
 
 module.exports = User;
--- app/src/model/UserStorage.js
+++ app/src/model/UserStorage.js
@@ -2,42 +2,28 @@
 
 const db = require("../config/db");
 
-class UserStorage {
-    static #getUserInfo(data, id) {
-        const users = JSON.parse(data);
-        const idx = users.id.indexOf(id);
-        const usersKeys = Object.keys(users); // => [id, password, name]
-        const userInfo = usersKeys.reduce((newUser, info) => {
-            newUser[info] = users[info][idx];
-            return newUser;
-        }, {});
-
-        return userInfo;
-    }
-
-    static #getUsers(data, isAll, fields) {
-        const users = JSON.parse(data);
-        if (isAll) return users;
-
-        const newUsers = fields.reduce((newUsers, field) => {
-            if (users.hasOwnProperty(field)) {
-                newUsers[field] = users[field];
-            }
-            return newUsers;
-        }, {});
-        return newUsers;
-    }
+class UserStorage { 
     
-    static getUsers(isAll, ...fields) {
-    }
-
     static getUserInfo(id) {
-        db.query("SELECT * FROM users", (err, data) => {
-            console.log(data);
+        return new Promise((resolve, reject) => {
+            const query = "SELECT * FROM person WHERE id = ?";
+            db.query(query, [id], (err, data) => {
+                if(err) reject(`${err}`);
+                console.log(data[0]);
+                resolve(data[0]);
+            });
         });
     }
 
     static async save(userInfo) {
+        return new Promise((resolve, reject) => {
+            const query = "INSERT INTO person(name, id, password, email) VALUES(?, ?, ?, ?)";
+            db.query(query,
+                [userInfo.name, userInfo.id, userInfo.password, userInfo.email], (err) => {
+                if(err) reject(`${err}`);
+                resolve({ success: true} );
+            });
+        });
     }
 }
 
